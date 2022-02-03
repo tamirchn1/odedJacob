c86a6645a4653cbb195dc9bb419bf9849868a1db
Tamirchn1@gmail.com <T2151991t>
2022-02-03 22:40:39 +0100

    images upload

--- src/components/Paintings.jsx
+++ src/components/Paintings.jsx
@@ -5,7 +5,11 @@ function Paintings() {
     <div class="content-container">
       <h1 class="">Paintings</h1>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
+        <img alt="oded's images" src={require("../images/1.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/2.jpg")} />
         <span class="px3 about-img">Text about the image</span>
       </div>
       <div class="img-block">
@@ -13,7 +17,31 @@ function Paintings() {
         <span class="px3 about-img">Text about the image</span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
+        <img alt="oded's images" src={require("../images/4.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/5.png")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/6.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/7.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/8.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/9.jpg")} />
+        <span class="px3 about-img">Text about the image</span>
+      </div>
+      <div class="img-block">
+        <img alt="oded's images" src={require("../images/10.jpg")} />
         <span class="px3 about-img">Text about the image</span>
       </div>
       <Footer />
