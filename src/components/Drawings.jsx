21ce1a5879d8cbf96c8c088db5148f7a374315d8
Tamirchn1@gmail.com <T2151991t>
2022-02-04 07:49:38 +0100

    uploaded images

--- src/components/Drawings.jsx
+++ src/components/Drawings.jsx
@@ -3,19 +3,26 @@ import Footer from "./Footer";
 function Drawings() {
   return (
     <div class="content-container">
-      <h1 class="">Drawing</h1>
+      <h1 class="">Drawings</h1>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/drawings/1.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/drawings/2.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
-      <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
-      </div>
+
       <Footer />
     </div>
   );
--- src/components/Paintings.jsx
+++ src/components/Paintings.jsx
@@ -5,44 +5,93 @@ function Paintings() {
     <div class="content-container">
       <h1 class="">Paintings</h1>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/1.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/1.jpg")} />
+        <span class="px3 about-img">
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/2.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/2.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/3.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/3.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/4.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/4.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/5.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/5.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/6.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/6.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/7.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/7.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/8.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/8.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/9.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/9.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <div class="img-block">
-        <img alt="oded's images" src={require("../images/10.jpg")} />
-        <span class="px3 about-img">Text about the image</span>
+        <img alt="oded's images" src={require("../images/paintings/10.jpg")} />
+        <span class="px3 about-img">
+          {" "}
+          Students, 2021 <br />
+          oil on canvas <br />
+          90cm/120cm
+        </span>
       </div>
       <Footer />
     </div>
