f14cc4e301025ed8e7ca42a378e7e578773c817d
Tamirchn1@gmail.com <T2151991t>
2022-02-07 17:19:57 +0100

    footer bottom

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,17 @@ function Home() {
   return (
     <div>
       <div>
-        <div class="index">
-          <img
-            class="index-img"
-            alt="oded's images"
-            src={require("../images/home-img.jpg")}
-          />
+        <div>
+          <div class="index">
+            <img
+              class="index-img"
+              alt="oded's images"
+              src={require("../images/home-img.jpg")}
+            />
+          </div>
         </div>
-      </div>
-      <Footer class="footer-index" />
+      </div>{" "}
+      <Footer />
     </div>
   );
 }
